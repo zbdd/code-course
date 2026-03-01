aa466f7787d7ff3e56744412a9b25c8d4cdeccfc
Zachary Durber <zdurber@gmail.com>
2026-03-01 09:33:02 +0800

    removed old lessons

--- old-lessons/lesson-001/completed/booking.ts
+++
@@ -1,8 +0,0 @@
-export type BookingRequest = {
-    start: number;
-    end: number;
-};
-
-export function isValidBooking(request: BookingRequest): boolean {
-    return request.start < request.end;
-}
--- old-lessons/lesson-001/completed/main.ts
+++
@@ -1,7 +0,0 @@
-import { isValidBooking } from "./booking.js";
-
-const valid = { start: 10, end: 12 };
-const invalid = { start: 14, end: 11 };
-
-console.log(isValidBooking(valid));   // true
-console.log(isValidBooking(invalid)); // false
